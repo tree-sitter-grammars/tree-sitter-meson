d5316de719bb65e87ad6c93217954d2f0c046e32
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2022-08-22 22:15:24 +0800

    feat(ifelse): support for if else
    Log: support for if else

--- grammar.js
+++ grammar.js
@@ -1,6 +1,6 @@
 const base = require("./grammer/base");
 const operation = require("./grammer/operation");
-
+const closure = require("./grammer/closure");
 module.exports = grammar({
   name: "meson",
 
@@ -8,14 +8,18 @@ module.exports = grammar({
     //source_file: ($) => repeat($._command_invocation),
     source_file: ($) =>
       repeat(
-        choice(
-          $.comment,
-          $.normal_command,
-          $.operatorunit,
-        ),
+        $._unit,
       ),
-
+    _unit: ($) =>
+      choice(
+        $.experession_statement,
+        $.comment,
+        $.normal_command,
+        $.operatorunit,
+        $.if_condition,
+      ),
     ...base,
     ...operation,
+    ...closure,
   },
 });
--- grammer/closure.js
+++ grammer/closure.js
@@ -1,23 +1,53 @@
-commands = [
-  "if",
-  "elseif",
-  "else",
-  "endif",
-  "foreach",
-  "endforeach",
-  "while",
-  "endwhile",
-  "function",
-  "endfunction",
-  "macro",
-  "endmacro",
-];
 module.exports = {
-  ...commandNames(...commands),
+  //...commandNames(...commands),
+  if_condition: ($) =>
+    seq(
+      $.if_command,
+      repeat($.elseif_command),
+      optional($.else_command),
+      "endif",
+    ),
+  if_command: ($) =>
+    seq(
+      "if",
+      repeat(/[\t ]/),
+      choice(
+        field("variable", $.identifier),
+        $.experession_statement,
+        $.operatorunit,
+        $.bool,
+      ),
+      optional(
+        repeat(
+          $._unit,
+        ),
+      ),
+    ),
+  elseif_command: ($) =>
+    seq(
+      "elif",
+      repeat(/[\t ]/),
+      choice(
+        field("variable", $.identifier),
+        $.experession_statement,
+        $.operatorunit,
+        $.bool,
+      ),
+      optional(
+        repeat(
+          $._unit,
+        ),
+      ),
+    ),
+  else_command: ($) =>
+    seq(
+      "else",
+      repeat(/[\t ]/),
+      optional(
+        repeat(
+          $._unit,
+        ),
+      ),
+    ),
 };
-function commandName(name) {
-  return { [name]: (_) => iregex(name) };
-}
-function commandNames(...names) {
-  return Object.assign({}, ...names.map(commandName));
-}
+
