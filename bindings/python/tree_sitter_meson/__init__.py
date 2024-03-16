59b1c64cc37e3c58a6bb7ca2b8630494ac47ca9b
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2024-03-16 21:33:26 +0800

    fix: to new tree sitter

---
+++ bindings/python/tree_sitter_meson/__init__.py
@@ -0,0 +1,5 @@
+"Meson grammar for tree-sitter"
+
+from ._binding import language
+
+__all__ = ["language"]
---
+++ bindings/swift/TreeSitterMeson/meson.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_MESON_H_
+#define TREE_SITTER_MESON_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_meson(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_MESON_H_
